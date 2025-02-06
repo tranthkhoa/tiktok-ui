1341312f7c8d70b2d73ec049b763407c6a3bbc09
mac <kakha12456@gmail.com>
2025-02-06 16:02:17 +0700

    06/02/2025 react-route-dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes'
+import { DefaultLayout } from '~/components/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          Learn React
-        </h1>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+            <Routes>
+                {
+                  publicRoutes.map((route, index) => {
+                  //  const Layout = route.layout === null ? Fragment : DefaultLayout;
+                  let Layout = DefaultLayout
+                  if (route.layout){
+                    Layout = route.layout
+                  }else if (route.layout === null){
+                    Layout = Fragment
+                  }
+                    const Page = route.component
+                    return <Route key={index} path={route.path} element={ <Layout><Page /></Layout> }/>
+                  })
+                }
+            </Routes>
+      </div>
+    </Router>
   );
 }
 
